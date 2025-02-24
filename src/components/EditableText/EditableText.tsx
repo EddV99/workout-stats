cebe7927d156449cd878b685cd7627bec1b85a22
Eduardo Valdivia <eddval0808@gmail.com>
2025-02-23 22:25:25 -0700

    small styling and EditableText component

---
+++ src/components/EditableText/EditableText.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from "react";
+import { MdEdit } from "react-icons/md";
+import Styles from "./EditableText.module.css";
+interface Props {
+  text: string;
+  setText: (t: string) => void;
+}
+function EditableText({ text, setText }: Props) {
+  const [editing, setEditing] = useState(false);
+  const editRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    editRef.current?.focus();
+  });
+
+  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && text) setEditing(false);
+  };
+
+  return (
+    <div className={Styles.container}>
+      {editing ? (
+        <>
+          <input
+            ref={editRef}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onKeyDown={handleEnter}
+            onBlur={() => {
+              if (text) setEditing(false);
+            }}
+          ></input>
+        </>
+      ) : (
+        <>
+          <button id={Styles.editButton} onClick={() => setEditing(true)}>
+            <MdEdit size={15} />
+          </button>
+          {text}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default EditableText;
--- src/components/Week/Week.tsx
+++ src/components/Week/Week.tsx
@@ -1,27 +1,32 @@
 import Styles from "./Week.module.css";
 import Day from "../Day/Day";
 import { useState } from "react";
+import EditableText from "../EditableText/EditableText";
 
 function Week() {
   const [title, setTitle] = useState("Workout");
 
   return (
-    <div id={Styles.container}>
-      <input
-        id={Styles.title}
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <div id={Styles.week}>
-        <Day day="1" />
-        <Day day="2" />
-        <Day day="3" />
-        <Day day="4" />
-        <Day day="5" />
-        <Day day="6" />
-        <Day day="7" />
+    <>
+      <div id={Styles.container}>
+        <EditableText text={title} setText={setTitle} />
+        <input
+          id={Styles.title}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <div id={Styles.week}>
+          <Day day="1" />
+          <Day day="2" />
+          <Day day="3" />
+          <Day day="4" />
+          <Day day="5" />
+          <Day day="6" />
+          <Day day="7" />
+        </div>
       </div>
-    </div>
+      <span id={Styles.viewDetails}>View Details</span>
+    </>
   );
 }
 
