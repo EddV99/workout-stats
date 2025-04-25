1336936c213cc8f41c0d1b2ad95e97507e51a13a
Eduardo Valdivia <eddval0808@gmail.com>
2025-04-25 14:10:33 -0600

    default data function

--- src/data/data.ts
+++ src/data/data.ts
@@ -19,10 +19,14 @@ export interface WorkoutData {
   sets: number;
 }
 
-export function makeDay(id: string, index: number, workouts: WorkoutData[]) {
+export function defaultData(): InternalData {
+  return { title: "Workout", days: [] };
+}
+
+export function makeDay(id: string, index: number, workouts: WorkoutData[]): DayData {
   return { id, index, workouts };
 }
 
-export function makeWorkout(id: string, name: string, group: Muscle, reps: number, sets: number) {
+export function makeWorkout(id: string, name: string, group: Muscle, reps: number, sets: number): WorkoutData {
   return { id, name, group, reps, sets };
 }
