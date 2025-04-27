7a54e4dd08437e26b0f5af5cdf7ea0de09a8c6db
Eduardo Valdivia <eddval0808@gmail.com>
2025-04-27 11:35:11 -0600

    exercise component improvements

--- src/components/Exercise/Exercise.tsx
+++ src/components/Exercise/Exercise.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useWorkoutData } from "../../context/WorkoutDataContext";
-import EditText from "../EditText/EditText";
+import Styles from "./Exercise.module.css";
+import { makeExercise } from "../../data/data";
 
 interface Props {
   id: string
@@ -9,14 +10,18 @@ interface Props {
 
 function Exercise({ id, index }: Props) {
   const { getExercise, updateExercise } = useWorkoutData();
-  const [edit, setEdit] = useState(false);
 
   let exercise = getExercise(id, index);
+  if (!exercise) return <div>ERROR: Couldn't find exercise</div>;
 
-  const handleNameChange = (text: string) => {
-    if (!exercise) return;
-    updateExercise(index, { ...exercise, name: text });
-  };
+  const [edit, setEdit] = useState(false);
+  const [exerciseEdit, setExerciseEdit] = useState({
+    name: exercise.name,
+    sets: exercise.sets,
+    reps: exercise.reps,
+    group: exercise.group
+  });
+
 
   const handleClickEdit = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -25,9 +30,23 @@ function Exercise({ id, index }: Props) {
 
   const handleClickCancel = () => {
     setEdit(false);
+    setExerciseEdit({
+      name: exercise.name,
+      sets: exercise.sets,
+      reps: exercise.reps,
+      group: exercise.group
+    })
   };
+
   const handleClickConfirm = () => {
     setEdit(false);
+    updateExercise(index, makeExercise(id, exerciseEdit.name, exerciseEdit.group, exerciseEdit.reps, exerciseEdit.sets));
+  };
+
+  const handleEditChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { name, value, type } = e.target;
+    setExerciseEdit(e => ({ ...e, [name]: type === 'number' ? Number(value) : value }));
   };
 
   return (
@@ -35,22 +54,21 @@ function Exercise({ id, index }: Props) {
       {
         exercise ?
           edit ? <>
-            <EditText text={exercise.name} setText={handleNameChange} />
-            <input type="number" value={exercise.sets} placeholder="Sets" />
-            <input type="number" value={exercise.reps} placeholder="Reps" />
+            <label htmlFor="name">Name</label>
+            <input name="name" onChange={handleEditChange} value={exerciseEdit.name} />
+            <label htmlFor="sets">Sets</label>
+            <input name="sets" onChange={handleEditChange} type="number" value={exerciseEdit.sets} placeholder="Sets" />
+            <label htmlFor="reps">Reps</label>
+            <input name="reps" onChange={handleEditChange} type="number" value={exerciseEdit.reps} placeholder="Reps" />
             <button onClick={handleClickConfirm}>Y</button>
             <button onClick={handleClickCancel}>N</button>
           </>
             :
-            <>
-              Name: {exercise.name}
-              Sets: {exercise.sets}
-              Reps: {exercise.reps}
-              Group: {exercise.group}
+            <div id={Styles.exercise}>
+              <div>{exercise.name}</div>
               <button onClick={handleClickEdit}>E</button>
-            </>
+            </div>
           : ""
-
       }
     </div>
   );
