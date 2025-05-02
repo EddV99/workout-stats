935d4fe07a8f951f7e4777231c89e2151cca3ecd
Eduardo Valdivia <eddval0808@gmail.com>
2025-05-01 22:52:48 -0600

    more workout details and styling

--- src/pages/DetailsPage/DetailsPage.tsx
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -37,7 +37,17 @@ function Content() {
 function Analytics() {
   const { workouts } = useWorkoutData();
 
-  const data = new Map<Muscle, { reps: number, sets: number, rest: number }>();
+  const data = new Map<Muscle, {
+    totalReps: number,
+    totalSets: number,
+    totalRest: number,
+    primaryReps: number,
+    primarySets: number,
+    primaryRest: number,
+    secondaryReps: number,
+    secondarySets: number,
+    secondaryRest: number,
+  }>();
 
   // parse data
   workouts.forEach(w => {
@@ -45,17 +55,29 @@ function Analytics() {
       e.primary.forEach(p => {
         let v = data.get(p);
         if (!v)
-          v = { reps: 0, sets: 0, rest: 0 };
-        v.reps += e.reps;
-        v.sets += e.sets;
+          v = {
+            totalReps: 0, totalSets: 0, totalRest: 0,
+            primaryReps: 0, primarySets: 0, primaryRest: 0,
+            secondaryReps: 0, secondarySets: 0, secondaryRest: 0,
+          };
+        v.totalReps += e.reps;
+        v.totalSets += e.sets;
+        v.primaryReps += e.reps;
+        v.primarySets += e.sets;
         data.set(p, v);
       });
       e.secondary.forEach(s => {
         let v = data.get(s);
         if (!v)
-          v = { reps: 0, sets: 0, rest: 0 };
-        v.reps += e.reps;
-        v.sets += e.sets;
+          v = {
+            totalReps: 0, totalSets: 0, totalRest: 0,
+            primaryReps: 0, primarySets: 0, primaryRest: 0,
+            secondaryReps: 0, secondarySets: 0, secondaryRest: 0,
+          };
+        v.totalReps += e.reps;
+        v.totalSets += e.sets;
+        v.secondaryReps += e.reps;
+        v.secondarySets += e.sets;
         data.set(s, v);
       });
     });
@@ -65,12 +87,21 @@ function Analytics() {
     {ALL_MUSCLE.map((mg) => {
       return data.has(mg) ? (
         <div id={Styles.group} key={mg}>
-          <div>{mg}</div>
-          <div>Sets: {data.get(mg)?.sets}</div>
-          <div>Reps: {data.get(mg)?.reps}</div>
+          <div id={Styles.name}>{mg}</div>
+          <div>Total Sets: {data.get(mg)?.totalSets}</div>
+          <div>Total Reps: {data.get(mg)?.totalReps}</div>
+          <div>Primary Target Sets: {data.get(mg)?.primarySets}</div>
+          <div>Primary Target Reps: {data.get(mg)?.primaryReps}</div>
+          <div>Secondary Target Sets: {data.get(mg)?.secondarySets}</div>
+          <div>Secondary Target Reps: {data.get(mg)?.secondaryReps}</div>
           <br />
         </div>
-      ) : "";
+      ) :
+        <div id={Styles.groupEmpty} key={mg}>
+          <div id={Styles.name}>{mg}</div>
+          Not Worked Out ❌
+          <br />
+        </div>;
     })}
   </div >;
 }
