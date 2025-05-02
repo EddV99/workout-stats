8615fca77222437ca3c81866416b90e32c68635b
Eduardo Valdivia <eddval0808@gmail.com>
2025-05-02 13:14:50 -0600

    show workouts in DetailsPage

--- src/pages/DetailsPage/DetailsPage.tsx
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -47,6 +47,8 @@ function Analytics() {
     secondaryReps: number,
     secondarySets: number,
     secondaryRest: number,
+    workoutNamesAsPrimary: Set<String>,
+    workoutNamesAsSecondary: Set<String>,
   }>();
 
   // parse data
@@ -59,11 +61,14 @@ function Analytics() {
             totalReps: 0, totalSets: 0, totalRest: 0,
             primaryReps: 0, primarySets: 0, primaryRest: 0,
             secondaryReps: 0, secondarySets: 0, secondaryRest: 0,
+            workoutNamesAsPrimary: new Set<String>(),
+            workoutNamesAsSecondary: new Set<String>(),
           };
         v.totalReps += e.reps;
         v.totalSets += e.sets;
         v.primaryReps += e.reps;
         v.primarySets += e.sets;
+        v.workoutNamesAsPrimary.add(e.name);
         data.set(p, v);
       });
       e.secondary.forEach(s => {
@@ -73,11 +78,14 @@ function Analytics() {
             totalReps: 0, totalSets: 0, totalRest: 0,
             primaryReps: 0, primarySets: 0, primaryRest: 0,
             secondaryReps: 0, secondarySets: 0, secondaryRest: 0,
+            workoutNamesAsPrimary: new Set<String>(),
+            workoutNamesAsSecondary: new Set<String>(),
           };
         v.totalReps += e.reps;
         v.totalSets += e.sets;
         v.secondaryReps += e.reps;
         v.secondarySets += e.sets;
+        v.workoutNamesAsSecondary.add(e.name);
         data.set(s, v);
       });
     });
@@ -87,14 +95,34 @@ function Analytics() {
     {ALL_MUSCLE.map((mg) => {
       return data.has(mg) ? (
         <div id={Styles.group} key={mg}>
-          <div id={Styles.name}>{mg}</div>
-          <div>Total Sets: {data.get(mg)?.totalSets}</div>
-          <div>Total Reps: {data.get(mg)?.totalReps}</div>
-          <div>Primary Target Sets: {data.get(mg)?.primarySets}</div>
-          <div>Primary Target Reps: {data.get(mg)?.primaryReps}</div>
-          <div>Secondary Target Sets: {data.get(mg)?.secondarySets}</div>
-          <div>Secondary Target Reps: {data.get(mg)?.secondaryReps}</div>
-          <br />
+          <div id={Styles.primary}>
+            <div id={Styles.name}>{mg}</div>
+            <div>Total Sets: {data.get(mg)?.totalSets}</div>
+            <div>Total Reps: {data.get(mg)?.totalReps}</div>
+            <div>Primary Target Sets: {data.get(mg)?.primarySets}</div>
+            <div>Primary Target Reps: {data.get(mg)?.primaryReps}</div>
+            <div>Secondary Target Sets: {data.get(mg)?.secondarySets}</div>
+            <div>Secondary Target Reps: {data.get(mg)?.secondaryReps}</div>
+          </div>
+          <div>
+            <div id={Styles.primary}>
+              <div className={Styles.underline}>Directly Hit With</div>
+              <div id={Styles.workouts}>
+                {Array.from(data.get(mg)?.workoutNamesAsPrimary || []).map((d) => {
+                  return <div>{d}</div>;
+                })}
+              </div>
+            </div>
+            <br />
+            <div id={Styles.secondary}>
+              <div className={Styles.underline}>In-Directly Hit With</div>
+              <div id={Styles.workouts}>
+                {Array.from(data.get(mg)?.workoutNamesAsSecondary || []).map((d) => {
+                  return <div>{d}</div>;
+                })}
+              </div>
+            </div>
+          </div>
         </div>
       ) :
         <div id={Styles.groupEmpty} key={mg}>
